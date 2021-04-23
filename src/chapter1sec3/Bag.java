f5cb3d6f48b37f193e470b57a23e2a840c1632ed
woshonigege666 <737001160@qq.com>
2021-04-23 21:00:26 +0800

    背包的链表实现

---
+++ src/chapter1sec3/Bag.java
@@ -0,0 +1,33 @@
+package chapter1sec3;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item>{
+    private int N;
+    private Node first;
+    private class Node{
+        Item item;
+        Node next;
+    }
+    public boolean isEmpty(){ return N == 0;}
+    public int size() {return N;}
+    public void add(Item item){
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+    public Iterator<Item> iterator(){
+        return new iteratorOfBag();
+    }
+    public class iteratorOfBag implements Iterator<Item>{
+        Node current = first;
+        public boolean hasNext(){return current != null;}
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+}
