bc62b14af5c3493a01ac7e1081cc75ccad3bdd79
woshonigege666 <737001160@qq.com>
2021-04-23 21:01:22 +0800

    队列的链表实现的测试用例

--- src/chapter1sec3/QueueTest.java
+++ src/chapter1sec3/QueueTest.java
@@ -6,27 +6,12 @@ public class QueueTest
 {
     public static void main(String[] args)
     {
-        LinkedQueue<String> s = new LinkedQueue<>();
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext())
-        {
-            String st = sc.next();
-            if(!(st.equals("-")))
-            {
-                s.enqueue(st);
-            }
-            else if(!s.isEmpty()) {
-                System.out.println(s.dequeue());
-            }
-        }
-        System.out.println("还剩" + s.size());
-        System.out.println(s.dequeue());
-        System.out.println(s.dequeue());
-        System.out.println(s.dequeue());
-        System.out.println(s.dequeue());
-        s.enqueue("st");
-        System.out.println(s.dequeue());
-
+        Queue<Integer> que = new Queue<>();
+        que.enqueue(2);
+        que.enqueue(3);
+        System.out.println(que.dequeue());
+        System.out.println(que.dequeue());
+        que.dequeue();//抛出异常
     }
 
 }
