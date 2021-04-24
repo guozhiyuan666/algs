717176e26789f4f9287a997e83d32120042ed0ec
woshonigege666 <737001160@qq.com>
2021-04-24 13:52:18 +0800

    用readInts的模板写一个readDates，测试用例

--- src/chapter1sec2/SmartDateTest.java
+++ src/chapter1sec2/SmartDateTest.java
@@ -2,10 +2,17 @@ package chapter1sec2;
 
 public class SmartDateTest {
     public static void main(String[] args) {
-        int d = 15;
-        int m = 13;
-        int y = 2031;
-        SmartDate a = new SmartDate(d, m, y);
-        System.out.println(a);
+//        int d = 15;
+//        int m = 10;
+//        int y = 2031;
+//        SmartDate a = new SmartDate(d, m, y);
+//        System.out.println(a);
+
+        //控制台输入：15/10/2021 15/10/2011 15/10/2001
+        SmartDate[] dateArray = SmartDate.readDates();
+        for(int i = 0; i < dateArray.length; i++){
+            System.out.println(dateArray[i]);
+        }
+
     }
 }
