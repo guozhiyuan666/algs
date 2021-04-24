ef0450f828a8bd896b430c7e59dc85457fd3f349
woshonigege666 <737001160@qq.com>
2021-04-24 11:59:13 +0800

    用数组实现队列并动态调整数组大小

---
+++ src/chapter1exercise3/ResizingArrayQueueOfStringTest.java
@@ -0,0 +1,21 @@
+package chapter1exercise3;
+
+public class ResizingArrayQueueOfStringTest {
+    public static void main(String[] args) {
+        ResizingArrayQueueOfStrings raqos = new ResizingArrayQueueOfStrings();
+        raqos.enqueue("1");
+        raqos.enqueue("2");
+        raqos.enqueue("3");
+
+        System.out.println(raqos.dequeue());
+        raqos.enqueue("4");
+        raqos.enqueue("5");
+
+        System.out.println(raqos.dequeue());
+        System.out.println(raqos.dequeue());
+        System.out.println(raqos.dequeue());
+        System.out.println(raqos.dequeue());
+
+
+    }
+}
