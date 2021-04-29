b3bbc451ac02d33618d0615a073f9aed50bc75ed
woshonigege666 <737001160@qq.com>
2021-04-29 11:13:36 +0800

    队列测试用例

--- src/chapter1sec3/QueueTest.java
+++ src/chapter1sec3/QueueTest.java
@@ -1,17 +1,20 @@
 package chapter1sec3;
 
-import java.util.Scanner;
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<Integer> queue1 = new Queue<>();
+        queue1.enqueue(1);
+        queue1.enqueue(2);
+        Queue<Integer> queue2 = new Queue<>();
+        queue2.enqueue(3);
+        queue2.enqueue(4);
+        queue1.catenation(queue2);
 
-public class QueueTest
-{
-    public static void main(String[] args)
-    {
-        Queue<Integer> que = new Queue<>();
-        que.enqueue(2);
-        que.enqueue(3);
-        System.out.println(que.dequeue());
-        System.out.println(que.dequeue());
-        que.dequeue();//抛出异常
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+        System.out.println(queue1.dequeue());
+
     }
 
 }
