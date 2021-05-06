8af648996aad4dd53460bbc32eb97268a203a5be
GZY <737001160@qq.com>
2021-05-06 22:05:23 +0800

    可迭代的栈+用构造函数得到一个栈的副本的测试用例

--- src/chapter1sec3/StackTest.java
+++ src/chapter1sec3/StackTest.java
@@ -3,11 +3,22 @@ package chapter1sec3;
 public class StackTest {
     public static void main(String[] args) {
         Stack<String> as = new Stack<>();
-        as.push("dd");
-        as.push("gg");
-        System.out.println(as.peek());
-        System.out.println(as.pop());
-        System.out.println(as.peek());
+        as.push("1");
+        as.push("2");
+        as.push("3");
+        as.push("4");
+        Stack<String> as2 = new Stack<>(as);
+
+        as2.pop();
+
+        for(String i : as){
+            System.out.println(i);//
+        }
+
+        for(String i : as2){
+            System.out.println(i);//
+        }
+
 
 
     }
