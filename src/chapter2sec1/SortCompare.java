85098f8be159fb4a1994d986636b6c20c54b7366
woshonigege666 <737001160@qq.com>
2021-05-07 22:08:15 +0800

    练习2.1.24 加入了插入排序的哨兵，用SortCompare测试其性能

--- src/chapter2sec1/SortCompare.java
+++ src/chapter2sec1/SortCompare.java
@@ -9,6 +9,7 @@ public class SortCompare {
         if(alg.equals("Selection")) Selection.sort(a);
         if(alg.equals("Insertion")) Insertion.sort(a);
         if(alg.equals("Shell")) Insertion.sort(a);
+        if(alg.equals("Num24")) Num24.sort(a);
         return timer.elapsedTime();
     }
     public static double timeRandomInput(String alg, int N, int M){//M个随机的大小为N的数组，排序M次
@@ -27,12 +28,14 @@ public class SortCompare {
         String alg1 = args[0];
         String alg2 = args[1];
         String alg3 = args[2];
-        int N = Integer.parseInt(args[3]);
-        int M = Integer.parseInt(args[4]);
+        String alg4 = args[3];
+        int N = Integer.parseInt(args[4]);
+        int M = Integer.parseInt(args[5]);
         double time1 = timeRandomInput(alg1, N, M);
         double time2 = timeRandomInput(alg2, N, M);
         double time3 = timeRandomInput(alg3, N, M);
-        System.out.printf("选择：%f, 插入:%f,希尔：%f", time1, time2,time3);
+        double time4 = timeRandomInput(alg4, N, M);
+        System.out.printf("选择：%f, 插入:%f, 希尔：%f, 插入哨兵:%f", time1, time2,time3,time4);
 
     }
 }
